6951134c8b92afed014262cc3f6b3b97b44168b9
ArakinJojo <davidkalok0911@gmail.com>
2024-05-24 09:42:16 +0200

    s

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -3,9 +3,18 @@
  */
 package controller;
 
+import model.Product;
+
 /**
  * 
  */
 public class ProductController {
 
+	public Product findProductByBarcode(int barcode) {
+		return Product;
+	}
+	
+	public void updateStockCount(int o.quantity, o.product) {
+		
+	}
 }
