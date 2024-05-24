d2f81257ebf1ebb077eb76ae5b6cdb4fb59c5e85
Kuskeren <166589219+Kuskeren@users.noreply.github.com>
2024-05-24 09:51:00 +0200

    Updated OrderLine
    Fixed et problem med getter og setter

--- src/model/Orderline.java
+++ src/model/Orderline.java
@@ -2,14 +2,26 @@ package model;
 
 public class Orderline {
 private int quantity; 
+
 private Product product; 
 private double linePrice; 
 
 public Orderline (int quantity, Product product) {
 	this.quantity = quantity; 
 	this.product = product;
-	this.linePrice = product.getCurrentPrice()*quantity;
-	//Er dette den bedste måde at sætte linePrice?? 
+	this.linePrice = product.getCurrentPrice()*quantity;}
+	
+public int getQuantity() {
+	return quantity;
+}
+
+
+public double getLinePrice() {
+	return linePrice;
+}
+
+public Product getProduct() {
+	return product;
 }
 
 }
