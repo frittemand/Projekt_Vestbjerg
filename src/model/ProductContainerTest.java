4bc3fd53e5a9adad528e0fe92c5154190847b7a0
VietTitans <mattnhudinh1@gmail.com>
2024-05-24 12:14:22 +0200

    Work in progress: Implementing test productcontainer

--- src/model/ProductContainerTest.java
+++ src/model/ProductContainerTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Test;
 class ProductContainerTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	Product findProductByBarcode(int i) {
+		//Arrange
+		Product product = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10 );
+		
+		//Act
+		result = findProductByBarcode(barcode);
+		
+		//Assert
+		assertEquals(product, result);
 	}
 
 }
