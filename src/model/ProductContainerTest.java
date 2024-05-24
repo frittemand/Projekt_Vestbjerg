4d97330f98126bc4729316514fcc1ba8d7558471
VietTitans <mattnhudinh1@gmail.com>
2024-05-24 13:38:12 +0200

    Update ProductContainerTest class. Still not working

--- src/model/ProductContainerTest.java
+++ src/model/ProductContainerTest.java
@@ -7,22 +7,19 @@ import org.junit.jupiter.api.Test;
 
 class ProductContainerTest {
 	private ProductContainer productContainer; 
-	
-	@Before
-	public void setUp() {
-		productContainer = new ProductContainer();
-		Product product = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10);
-		productContainer.addProduct(product);
-	}
 
 	@Test
 	void findProductByBarcode(int i) {
 		//Arrange
+		productContainer = new ProductContainer();
+		Product product = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10);
+		productContainer.addProduct(product);
+		
 		Product expectedProduct = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10);
-		int barcode = expectedProduct.getBarcode();
 		
 		//Act
-		Product result = productContainer.findProductByBarcode(12345);
+		int barcode = expectedProduct.getBarcode();
+		Product result = productContainer.findProductByBarcode(barcode);
 		
 		//Assert
 		assertEquals(expectedProduct, result);
