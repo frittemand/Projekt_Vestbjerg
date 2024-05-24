e8955b5d930320af8a14477a23b3d2c8319dc9c4
VietTitans <mattnhudinh1@gmail.com>
2024-05-24 13:12:31 +0200

    Pushing for David

--- src/model/ProductContainerTest.java
+++ src/model/ProductContainerTest.java
@@ -11,18 +11,18 @@ class ProductContainerTest {
 	@Before
 	public void setUp() {
 		productContainer = new ProductContainer();
-		Product product = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10 );
+		Product product = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10);
 		productContainer.addProduct(product);
 	}
 
 	@Test
 	void findProductByBarcode(int i) {
 		//Arrange
-		Product expectedProduct = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10 );
+		Product expectedProduct = new Product (12345,10,"Hylde 1, række 1",10,11,8,"Dette er en hammer","hammer",100,10);
+		int barcode = expectedProduct.getBarcode();
 		
 		//Act
-		int barcode = 12345;
-		Product result = productContainer.findProductByBarcode(barcode);
+		Product result = productContainer.findProductByBarcode(12345);
 		
 		//Assert
 		assertEquals(expectedProduct, result);
