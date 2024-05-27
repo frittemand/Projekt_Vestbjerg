a56f5562a1e973e4f0c4594c0527e5e2e957299f
VietTitans <mattnhudinh1@gmail.com>
2024-05-27 21:42:29 +0200

    ProductTest: Ready for review
    Implemented test. Ready for review. OBS: line 24 - pr.getDataFrom() - Not sure if it's the correct parameter to give

--- src/model/ProductTest.java
+++ src/model/ProductTest.java
@@ -3,6 +3,8 @@ package model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.Stack;
+
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
@@ -10,10 +12,16 @@ class ProductTest {
 	@Test
 	void testGetPurchasePrice() {
 		//Arrange
-		Product p1 = new Product (12345, 10, "Hylde 1, række 1", 10, 11, 8, "Dette er en hammer", "hammer", 100, 10);
+		Stack<Price> purchasePriceLog = new Stack<>();
+		Product p1 = new Product (12345, 10, "Hylde 1, række 1", 10, 11, 8, 
+				"Dette er en hammer", "hammer", 100, 10);
+		Price pr1 = new Price(p1.getPurchasePrice());
+		purchasePriceLog.push(pr1);
+		
+		Stack<Price> tempStack = (Stack<Price>)purchasePriceLog.clone();
 		
 		//Act
-		LocalDate targetPriceLogDate = (LocalDate priceLogDate); 
+		LocalDate targetPriceLogDate = (pr1.getDateFrom()); 
 		double result = p1.getPurchasePrice(targetPriceLogDate);
 		
 		//Assert
