a8365f26dd71b42be8a1157390a5f7b1083ac79c
VietTitans <mattnhudinh1@gmail.com>
2024-05-28 21:57:26 +0200

    Create LoanUI.java
    Created STUMB

---
+++ src/ui/LoanUI.java
@@ -0,0 +1,23 @@
+package ui;
+
+import controller.LoanController;
+
+public class LoanUI {
+	private LoanController loanController;
+	
+	public void findCustomerByPhoneNumber(int phoneNumber) {
+		//TODO implement method
+	}
+	
+	public void createLoan(int duration) {
+		//TODO implement method
+	}
+	
+	public void addCopyToLoan(String toolName) {
+		//TODO implement method
+	}
+	
+	public void endLoan() {
+		//TOOD implement method
+	}
+}
