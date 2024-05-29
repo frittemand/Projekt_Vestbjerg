78df28b2ab5078e6728b23e0e977aa52f685f893
ArakinJojo <davidkalok0911@gmail.com>
2024-05-29 08:30:51 +0200

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -2,17 +2,18 @@ package model;
 
 public class Customer extends Person {
 
-	private int customerNumber;
+	private static int customerNumber = 0;
 	
 	
 	public Customer(String name, String address, String email, int phoneNumber) {
 		this.name = name;
 		this.address = address;
 		this.email = email;
-		customerNumber = phoneNumber;
+		this.phoneNumber = phoneNumber;
+		this.customerNumber = customerNumber++;
 	}
 	
 	public int getPhoneNumber() {
-		return customerNumber;
+		return phoneNumber;
 	}
 }
