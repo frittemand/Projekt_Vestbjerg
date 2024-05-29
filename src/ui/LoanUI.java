184afe13c841af9e1e1e04263f9a02b4e22a17ea
Kuskeren <166589219+Kuskeren@users.noreply.github.com>
2024-05-29 08:31:42 +0200

    Skelet til LoanUI

--- src/ui/LoanUI.java
+++ src/ui/LoanUI.java
@@ -1,11 +1,66 @@
 package ui;
 
+import java.util.Scanner;
+
 import controller.CustomerController;
 import controller.LoanController;
 
 public class LoanUI {
 	private LoanController loanController;
 	private CustomerController customerController; //TODO Add this attribute to the design class diagram
+	
+public LoanUI(){
+	loanController = new LoanController(); 
+	customerController = new CustomerController();
+}
+	
+public void start() {
+	loanMenu();
+		}
+
+public void loanMenu() {
+	boolean running = true;
+	while (running) {
+		int choice = writeLoanMenu();
+		switch (choice) {
+		case 1:
+			createLoan();
+			addCopyToLoan();
+			break;
+		
+		case 2:
+			System.out.println("not implemented");
+			break;
+		
+		case 3:
+			
+			break;
+		
+		case 0:
+			endLoan();
+			running = false;
+			break;
+		
+		default:
+			System.out.println("Not valid input.");
+			break;
+		}	
+	}
+}
+
+private int writeLoanMenu() {
+	Scanner keyboard = new Scanner(System.in);
+	System.out.println("** Sale Menu **");
+	System.out.println(" (1) Create Sale");
+	System.out.println(" (2) Not implemented");
+	System.out.println(" (3) Make Payment");
+	System.out.println(" (0) Finish");
+	System.out.print("\n Choose: ");
+	int choice = getIntegerFromUser(keyboard);
+	return choice;
+
+}	
+	
 	
 	public void findCustomerByPhoneNumber(int phoneNumber) {
 		customerController.findCustomerByPhoneNumber(phoneNumber);
