8a79006248bf0e59a84acdfaee856f71a7b5f09c
frittemand <150781905+frittemand@users.noreply.github.com>
2024-05-29 10:53:58 +0200

    refactor navne i tryme

--- src/ui/TryMe.java
+++ src/ui/TryMe.java
@@ -10,74 +10,78 @@ import model.ProductContainer;
 import model.ToolContainer;
 import model.ToolCopy;
 public class TryMe {
-    private ProductContainer pc;
-    private CustomerContainer cc;
-    private Product p1, p2, p3, p4, p5;
-    private Customer c1, c2, c3, c4, c5;
-    private Tool t1, t2, t3;
+    private ProductContainer productContainer;
+    private CustomerContainer customerContainer;
+    private ToolContainer toolContainer;
+    private Product product1, product2, product3, product4, product5;
+    private Customer customer1, customer2, customer3, customer4, customer5;
+    private Tool tool1, tool2, tool3;
     private ToolCopy copy1, copy2, copy3, copy4, copy5, copy6;
-	private ToolContainer tc;
+	
 
     public TryMe() { 
-        pc = ProductContainer.getInstance();
+        productContainer = ProductContainer.getInstance();
         
-        p1 = new Product (12345, 10, "Hylde 1, række 1", 10, 11, 8, "Dette er en hammer", "hammer", 100, 10);
-        	pc.addProduct(p1);
+        product1 = new Product (12345, 10, "Hylde 1, række 1", 10, 11, 8, "Dette er en hammer", "hammer", 100, 10);
+        	productContainer.addProduct(product1);
         
-        p2 = new Product (22345, 10, "Hylde 1, række 2", 16, 11, 8, "Dette er en lille hammer", "hammer", 100, 10);
-        	pc.addProduct(p2);
+        product2 = new Product (22345, 10, "Hylde 1, række 2", 16, 11, 8, "Dette er en lille hammer", "hammer", 100, 10);
+        	productContainer.addProduct(product2);
         
-        p3 = new Product (32345, 10, "Hylde 1, række 3", 15, 11, 8, "Dette er en skruetrækker", "skruetrækker", 100, 10);
-        	pc.addProduct(p3);
+        product3 = new Product (32345, 10, "Hylde 1, række 3", 15, 11, 8, "Dette er en skruetrækker", "skruetrækker", 100, 10);
+        	productContainer.addProduct(product3);
         
-        p4 = new Product (42345, 10, "Hylde 1, række 4", 15, 11, 8, "Dette er et målebånd", "målebånd", 100, 10);
-        	pc.addProduct(p4);
+        product4 = new Product (42345, 10, "Hylde 1, række 4", 15, 11, 8, "Dette er et målebånd", "målebånd", 100, 10);
+        	productContainer.addProduct(product4);
         
-        p5 = new Product (52345, 10, "Hylde 1, række 5", 110, 11, 8, "Dette er en boremaskine", "boremaskine", 100, 10);
-        	pc.addProduct(p5);
-        
-        
-        cc = CustomerContainer.getInstance();
-        
-        c1 = new Customer ("Mikkel", "Karl Marx alle 87", "mikkel@hotmail.com", 22407778);
-        	cc.addCustomerToContainer(c1);
-        
-        c2 = new Customer ("lars", "hjemme hos dig", "LarsErEnHyggefætter@gmail.com", 12345568);
-        	cc.addCustomerToContainer(c2);
-        
-        c3 = new Customer ("Peter", "aalborg kongress", "peterkok@gmail.com", 49385135);
-        	cc.addCustomerToContainer(c3);
-        
-        c4 = new Customer ("kasper", "hjemløs", "JegHaderKøbenhavn@gmail.com", 27561359);
-        	cc.addCustomerToContainer(c4);
-        
-        c5 = new Customer ("Stefan", "Store Villavej", "LadMigVære@gmail.com", 14359768);
-        	cc.addCustomerToContainer(c5);
+        product5 = new Product (52345, 10, "Hylde 1, række 5", 110, 11, 8, "Dette er en boremaskine", "boremaskine", 100, 10);
+        	productContainer.addProduct(product5);
         
         
-        tc = ToolContainer.getInstance();
+        customerContainer = CustomerContainer.getInstance();
         
-        t1 = new Tool (47, "flotte røde plæneklippere", 11111, "plæneklipper");
-        	tc.addToolToToolContainer(t1);
+        customer1 = new Customer ("Mikkel", "Karl Marx alle 87", "mikkel@hotmail.com", 22407778);
+        	customerContainer.addCustomerToContainer(customer1);
+        
+        customer2 = new Customer ("lars", "hjemme hos dig", "LarsErEnHyggefætter@gmail.com", 12345568);
+        	customerContainer.addCustomerToContainer(customer2);
+        
+        customer3 = new Customer ("Peter", "aalborg kongress", "peterkok@gmail.com", 49385135);
+        	customerContainer.addCustomerToContainer(customer3);
+        
+        customer4 = new Customer ("kasper", "hjemløs", "JegHaderKøbenhavn@gmail.com", 27561359);
+        	customerContainer.addCustomerToContainer(customer4);
+        
+        customer5 = new Customer ("Stefan", "Store Villavej", "LadMigVære@gmail.com", 14359768);
+        	customerContainer.addCustomerToContainer(customer5);
+        
+        
+        toolContainer = ToolContainer.getInstance();
+        
+        tool1 = new Tool (47, "flotte røde plæneklippere", 11111, "plæneklipper");
+        	toolContainer.addToolToToolContainer(tool1);
         		copy1 = new ToolCopy ();
         		copy2 = new ToolCopy ();
-        			t1.addCopyToTool(copy1);
-        			t1.addCopyToTool(copy2);
+        			tool1.addCopyToTool(copy1);
+        			tool1.addCopyToTool(copy2);
         
-        t2 = new Tool (4, "store sorte skruetrækkere", 22222, "skruetrækker");
-        	tc.addToolToToolContainer(t2);
+        tool2 = new Tool (4, "store sorte skruetrækkere", 22222, "skruetrækker");
+        	toolContainer.addToolToToolContainer(tool2);
         		copy3 = new ToolCopy ();
         		copy4 = new ToolCopy ();
-        			t2.addCopyToTool(copy3);
-        			t2.addCopyToTool(copy4);
+        			tool2.addCopyToTool(copy3);
+        			tool2.addCopyToTool(copy4);
         
-        t3 = new Tool (23, "små gule motorsave", 33333, "motorsav");
-        	tc.addToolToToolContainer(t3);
+        tool3 = new Tool (23, "små gule motorsave", 33333, "motorsav");
+        	toolContainer.addToolToToolContainer(tool3);
         		copy5 = new ToolCopy ();
         		copy6 = new ToolCopy ();
-        			t3.addCopyToTool(copy5);
-        			t3.addCopyToTool(copy6);
+        			tool3.addCopyToTool(copy5);
+        			tool3.addCopyToTool(copy6);
  
-}
+    }
 
+    public static void main(String[] args) {
+        TryMe tryMe = new TryMe();	
+    }
 }
