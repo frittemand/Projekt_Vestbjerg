e8fb97afdf55f987c3abba8eea7aec5798c66921
VietTitans <mattnhudinh1@gmail.com>
2024-05-29 12:52:54 +0200

    Formatting Price

--- src/model/Price.java
+++ src/model/Price.java
@@ -3,27 +3,28 @@ package model;
 import java.time.LocalDate;
 
 public class Price {
-private double value;
-private LocalDate dateFrom;
+	private double value;
+	private LocalDate dateFrom;
 
+	public Price(double value) {
+		this.value = value;
+		this.dateFrom = LocalDate.now();
+	}
 
-public Price(double value) {
-	this.value = value;
-	this.dateFrom =LocalDate.now();
-}
+	public double getValue() {
+		return value;
+	}
 
-public double getValue() {
-	return value;
-}
-public void setValue(double value) {
-	this.value = value;
-}
-public LocalDate getDateFrom() {
-	return dateFrom;
-}
-public void setDateFrom(LocalDate dateFrom) {
-	this.dateFrom = dateFrom;
-}
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public LocalDate getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(LocalDate dateFrom) {
+		this.dateFrom = dateFrom;
+	}
 
 }
-
