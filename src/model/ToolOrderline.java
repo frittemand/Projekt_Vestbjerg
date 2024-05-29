9a9af50810efe50549052caa0fce4f5b6ccb4168
Kuskeren <166589219+Kuskeren@users.noreply.github.com>
2024-05-29 12:45:43 +0200

    update ToolOrderline
    tilføjer nye attributter og get metoder

--- src/model/ToolOrderline.java
+++ src/model/ToolOrderline.java
@@ -4,10 +4,13 @@ public class ToolOrderline {
 	private double linePrice; 
 	private ToolCopy copy;
 	private double toolDayRate;
+	private String toolName;
 	
-	public ToolOrderline(ToolCopy copy, double dayRate) {
+	public ToolOrderline(ToolCopy copy, double dayRate, double linePrice, String toolName) {
 		this.copy = copy;
+		this.linePrice = linePrice;
 		this.toolDayRate = dayRate; 
+		this.toolName = toolName;
 	}
 	
 	public double getLinePrice() {
@@ -20,6 +23,9 @@ public class ToolOrderline {
 
 	public double getToolDayRate() {
 		return toolDayRate;
+	}
+	public String getToolName() {
+		return toolName;
 	}
 
 }
