889dfac78f021695251698b0456856daf04e6267
VietTitans <mattnhudinh1@gmail.com>
2024-05-29 12:45:16 +0200

    Refactoring: Formatting

--- src/model/ToolContainer.java
+++ src/model/ToolContainer.java
@@ -4,63 +4,62 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class ToolContainer {
-	
-		private static ToolContainer INSTANCE;  
-		private ArrayList<Tool> tools;
 
+	private ArrayList<Tool> tools;
+	private static ToolContainer INSTANCE;
 
-		public ToolContainer() {
-			tools = new ArrayList<>();
+	public ToolContainer() {
+		tools = new ArrayList<>();
+	}
+
+	public static ToolContainer getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new ToolContainer();
 		}
+		return INSTANCE;
+	}
 
-		public static ToolContainer getInstance() {
-			if (INSTANCE == null) {
-				INSTANCE = new ToolContainer();
-			}
-			return INSTANCE;
-		}
+	public void addToolToToolContainer(Tool tool) {
+		tools.add(tool);
+	}
 
-		public void addToolToToolContainer(Tool tool) {
-			tools.add(tool);
-		}
+	public ToolCopy findToolByToolName(String toolName) {
+		ToolCopy foundCopy = null;
+		boolean searching = true;
+		Iterator<Tool> it = tools.iterator();
 
-		public ToolCopy findToolByToolName(String toolName) {
-				ToolCopy foundCopy = null;
-				boolean searching = true;
-				Iterator<Tool> it = tools.iterator();
-				
-				while(it.hasNext() && searching) {
-					Tool tool = it.next();
-					if(tool.getToolName().equalsIgnoreCase(toolName)){
-						foundCopy =tool.findCopyByIsHome();
-						searching = false;
-						
-					}
-				}
-				return foundCopy;
-		}
-		
-		public void updateIsHome(ToolCopy copy) {
-			if(copy != null) {
-				copy.setHome(false);
+		while (it.hasNext() && searching) {
+			Tool tool = it.next();
+			if (tool.getToolName().equalsIgnoreCase(toolName)) {
+				foundCopy = tool.findCopyByIsHome();
+				searching = false;
+
 			}
 		}
-		public double findDayRate(String toolName) {
-			double foundDayRate = 0;
-			boolean searching = true;
-			
-			Iterator<Tool> it = tools.iterator();
-			
-			while(it.hasNext() && searching) {
-				Tool tool = it.next();
-				if(tool.getToolName().equalsIgnoreCase(toolName)){
-					foundDayRate = tool.getDayRate(); 
-					searching = false;
-					
-				}
-			}
-			return foundDayRate;
-		
-		}
-		}
+		return foundCopy;
+	}
 
+	public void updateIsHome(ToolCopy copy) {
+		if (copy != null) {
+			copy.setHome(false);
+		}
+	}
+
+	public double findDayRate(String toolName) {
+		double foundDayRate = 0;
+		boolean searching = true;
+
+		Iterator<Tool> it = tools.iterator();
+
+		while (it.hasNext() && searching) {
+			Tool tool = it.next();
+			if (tool.getToolName().equalsIgnoreCase(toolName)) {
+				foundDayRate = tool.getDayRate();
+				searching = false;
+
+			}
+		}
+		return foundDayRate;
+
+	}
+}
