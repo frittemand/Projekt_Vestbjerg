f290ba723772bc25fa8730119acddeda547bb428
VietTitans <mattnhudinh1@gmail.com>
2024-05-29 13:02:02 +0200

    Update format for Orderline

--- src/model/Orderline.java
+++ src/model/Orderline.java
@@ -3,26 +3,26 @@ package model;
 import java.time.LocalDate;
 
 public class Orderline {
-private int quantity; 
-private Product product; 
-private double linePrice; 
+	private int quantity;
+	private Product product;
+	private double linePrice;
 
-public Orderline (int quantity, Product product) {
-	this.quantity = quantity; 
-	this.product = product;
-	this.linePrice = product.getCurrentPrice(LocalDate.now())*quantity;
-}
-public int getQuantity() {
-	return quantity;
-}
+	public Orderline(int quantity, Product product) {
+		this.quantity = quantity;
+		this.product = product;
+		this.linePrice = product.getCurrentPrice(LocalDate.now()) * quantity;
+	}
 
+	public int getQuantity() {
+		return quantity;
+	}
 
-public double getLinePrice() {
-	return linePrice;
-}
+	public double getLinePrice() {
+		return linePrice;
+	}
 
-public Product getProduct() {
-	return product;
-}
+	public Product getProduct() {
+		return product;
+	}
 
 }
