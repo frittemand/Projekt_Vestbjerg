e360d9afe40be02ba2e7cae92214644bf8e7f7e9
TooManyLoops <10404844@ucn.dk>
2024-05-30 19:26:45 +0200

    update product comments
    tilføjet comments til stacks of constructor

--- src/model/Product.java
+++ src/model/Product.java
@@ -61,9 +61,9 @@ public class Product {
 	private int minStock;
 
 	/**
-	 * basePriceLog creates a new stack
-	 * currentPriceLog creates a new stack
-	 * pruchasePriceLog creates a new stack
+	 * basePriceLog creates a new stack consisting of Price objects
+	 * currentPriceLog creates a new stack consisting of Price objects
+	 * pruchasePriceLog creates a new stack consisting of Price objects
 	 * @param barcode is a products barcode
 	 * @param stockCount is how many of a product is in inventory
 	 * @param locationNumber is where the product is physically located
@@ -74,10 +74,22 @@ public class Product {
 	 * @param itemName is the name of a product
 	 * @param maxStock is the maximum amount of a product that is allowed to be stored in the inventory
 	 * @param minStock is the minimum amount of a product before a restock must happen
-	 * pr1 adds a basePrice to basePriceLog stack
-	 * pr2 adds a currentPrice to currentPriceLog stack
-	 * pr adds a purchasePrice to purchasePriceLog stack
 	 * 
+	 * 
+	 */
+	
+	/*
+	 * constructor for Product
+	 * pr1 stores new Price with purchasePrice as input
+	 * pr2 stores new Price with purchasePrice as input
+	 * pr3 stores new Price with purchasePrice as input
+	 * 
+	 * creates a new Product object aswell as 3 Price objects associated to the Product.
+	 * 3 stacks basePriceLog, currentPriceLog and purchasePriceLog are initialized.
+	 * the 3 prices are stored as local variables, and pushed to the top of a stack.
+	 * theres 1 stack for each price. New Price with input basePrice, goes to the top of stack basePriceLog.
+	 * Price with input currentPrice goes on top of stack currentPriceLog and so on.
+	 *
 	 */
 	public Product(int barcode, int stockCount, String locationNumber, double basePrice, double currentPrice,
 			double purchasePrice, String description, String itemName, int maxStock, int minStock) {
