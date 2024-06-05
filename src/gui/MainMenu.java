6376a708b7f7ef7659510c24c50160c77d84920f
VietTitans <mattnhudinh1@gmail.com>
2024-06-05 11:12:11 +0200

    Add empty eventlisteners to buttons

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -5,6 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainMenu extends JFrame {
 
@@ -37,6 +46,91 @@ public class MainMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton btnSaleMenu = new JButton("Sale menu");
+		btnSaleMenu.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnSaleMenu = new GridBagConstraints();
+		gbc_btnSaleMenu.insets = new Insets(0, 0, 5, 0);
+		gbc_btnSaleMenu.gridx = 5;
+		gbc_btnSaleMenu.gridy = 1;
+		panel.add(btnSaleMenu, gbc_btnSaleMenu);
+		
+		JButton btnLendMenu = new JButton("Lend menu");
+		btnLendMenu.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnLendMenu = new GridBagConstraints();
+		gbc_btnLendMenu.insets = new Insets(0, 0, 5, 0);
+		gbc_btnLendMenu.gridx = 5;
+		gbc_btnLendMenu.gridy = 2;
+		panel.add(btnLendMenu, gbc_btnLendMenu);
+		
+		JButton btnProducts = new JButton("Products");
+		btnProducts.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnProducts = new GridBagConstraints();
+		gbc_btnProducts.insets = new Insets(0, 0, 5, 0);
+		gbc_btnProducts.gridx = 5;
+		gbc_btnProducts.gridy = 3;
+		panel.add(btnProducts, gbc_btnProducts);
+		
+		JButton btnGenerateTestData = new JButton("Generate Test Data");
+		btnGenerateTestData.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnGenerateTestData = new GridBagConstraints();
+		gbc_btnGenerateTestData.insets = new Insets(0, 0, 5, 0);
+		gbc_btnGenerateTestData.gridx = 5;
+		gbc_btnGenerateTestData.gridy = 4;
+		panel.add(btnGenerateTestData, gbc_btnGenerateTestData);
+		
+		JLabel lblNewLabel = new JLabel("Main menu");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{81, 89, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{23, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnCancel.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+		gbc_btnCancel.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCancel.anchor = GridBagConstraints.NORTHEAST;
+		gbc_btnCancel.gridx = 0;
+		gbc_btnCancel.gridy = 0;
+		panel_1.add(btnCancel, gbc_btnCancel);
 	}
 
 }
