d36726a36c6eed1bc1da8bc2e4c1d8e9538c66be
ArakinJojo <davidkalok0911@gmail.com>
2024-06-11 13:07:08 +0200

    extraMethod

---
+++ src/ExtraMethods/extraMethods.java
@@ -0,0 +1,17 @@
+package ExtraMethods;
+
+public class extraMethods {
+	
+	public void printReceit() {
+		System.out.println("Vestbjerg Byggecenter");
+		System.out.println("Receipt");
+		System.out.println("xxxxxxxxxxxxxx");
+		System.out.println("Name: ");
+		System.out.println("Loan items: ");
+		System.out.println(getToolName());
+		System.out.println(getDayRate());
+		System.out.println("Duration: ");
+		
+	}
+
+}
